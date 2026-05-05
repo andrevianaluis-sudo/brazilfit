0ab0f5c1b5862fe434c9742fa9028819ba5c71e0
Andre Viana <andre.vianaluis@gmail.com>
2026-05-05 17:14:31 +0100

    feat: add Start Routine button to rest_day cards

---
+++ fix41.js
@@ -0,0 +1,18 @@
+﻿const fs = require("fs");
+const file = "C:/Users/viana/BRAZILFIT/frontend/src/pages/client/ClientWellness.jsx";
+let c = fs.readFileSync(file, "utf8");
+
+// Fix line 499-500 (rest_day section)
+c = c.replace(
+  `                  onStart={()=>{if(item.type===""breathing"")setBreathingSession(item);else setActiveSession(item);}}\n                  onStartRoutine={item.type===""rest_day""?()=>setStretchRoutineSession(item):null}/>`,
+  `                  onStart={()=>{if(item.type==='breathing')setBreathingSession(item);else setActiveSession(item);}}\n                  onStartRoutine={item.type==='rest_day'?()=>setStretchRoutineSession(item):null}/>`
+);
+
+// Fix line 548-549 (other section)  
+c = c.replace(
+  `                onStart={()=>{if(item.type==='breathing')setBreathingSession(item);else setActiveSession(item);}}\n                onStartRoutine={item.type==='rest_day'?()=>setStretchRoutineSession(item):null}/>`,
+  `                onStart={()=>{if(item.type==='breathing')setBreathingSession(item);else setActiveSession(item);}}\n                onStartRoutine={item.type==='rest_day'?()=>setStretchRoutineSession(item):null}/>`
+);
+
+fs.writeFileSync(file, c, "utf8");
+console.log("Done");
---
+++ fix42.js
@@ -0,0 +1,11 @@
+﻿const fs = require("fs");
+const file = "C:/Users/viana/BRAZILFIT/frontend/src/pages/client/ClientWellness.jsx";
+let c = fs.readFileSync(file, "utf8");
+
+c = c.replace(
+  `                  onStartRoutine={item.type==="rest_day"?()=>setStretchRoutineSession(item):null}/>\n            </div>`,
+  `                  onStartRoutine={item.type==="rest_day"?()=>setStretchRoutineSession(item):null}/>\n              ))}\n            </div>`
+);
+
+fs.writeFileSync(file, c, "utf8");
+console.log("Done");
